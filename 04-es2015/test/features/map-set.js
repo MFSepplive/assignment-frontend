9ce9d60ac0f4d3f1330641d6571de85fedf51bd7
Markus <mfrenslich.mmt-b2015@fh-salzburg.ac.at/Users/Maxi/WebGitWorkspace/markus_frenslich_wp2>
2016-11-24 12:22:09 +0100

    finished v3.0

--- 04-es2015/test/features/map-set.js
+++ 04-es2015/test/features/map-set.js
@@ -28,6 +28,22 @@ export default function() {
   })
 
   it('should show Map/Set behavior in ES6', () => {
+    const s = new Set();
+    s.add(1);
+    s.add(2);
+    s.add(3);
 
+    s.has(1).should.be.true;
+    s.has(2).should.be.true;
+    s.has(3).should.be.true;
+    s.has(4).should.be.false;
+
+    const m = new Map();
+    m.set("Hello", "World!");
+    m.set("second", 2);
+    m.set(1,2);
+
+    m.get("Hello").should.eql("World!");
+    m.get("second").should.eql(2);
   })
 }
